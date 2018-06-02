79145d053d75c9045c7376ccd6dc75658ba75236
Cirius1792 <c.tecce1@studenti.unisa.it>
2018-06-02 14:52:56 +0200

    first commit

---
+++ WirelessAccessPoint/my_first_problem.py
@@ -0,0 +1,30 @@
+import pygmo as pg
+
+class my_problem:
+    def __init__(self, dim=10):
+        self.dim = dim
+
+
+    def fitness(self, x):
+        """The fitness() method is expected to return the fitness of the input decision vector """
+        return [sum(x * x)]
+
+
+
+    def get_bounds(self):
+        """ get_bounds() is expected to return the box bounds of the problem, (lb,ub), which also implicitly define the dimension of the problem"""
+        return ([-1] * self.dim, [1] * self.dim)
+
+
+if __name__ == '__main__':
+    prob = pg.problem(my_problem(3))
+    print(prob)
+    #class pygmo.de(gen = 1, F = 0.8, CR = 0.9, variant = 2, ftol = 1e-6, xtol = 1e-6, seed = random)
+    gen = 500
+    F = 0.8
+    CR = 0.9
+    seed = 7
+    algo = pg.algorithm(pg.de(gen,F,CR))
+    pop = pg.population(prob, 10)
+    pop = algo.evolve(pop)
+    print(pop.champion_f)
